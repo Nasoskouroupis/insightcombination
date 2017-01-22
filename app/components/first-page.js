7418381a7e560702ab30150db49c441c6e383346
stefano kitano <S.Kouroupis@gmail.com>
2017-01-22 11:56:54 +0000

    almost there

--- app/components/first-page.js
+++ app/components/first-page.js
@@ -1,9 +1,22 @@
 app.component("firstPage", {
     templateUrl: "app/templates/first-page.html",
+    bindings: {
+        $router: "<"
+    },
     controllerAs: "model",
-    controller: function () {
+    controller: function ($routeParams, session) {
         var model = this;
+        model.results = [];
         model.$onInit = function () {
+            model.dataset = session.getDataset();
+            var r1 = model.random(0, mmodel.dataset);
+            var r2 = model.random(0, mmodel.dataset);
+            model.results.push(model.dataset[r1]);
+            model.results.push(model.dataset[r2]);
         };
+        model.random = function (min, max) {
+            let rand = Math.floor(Math.random() * (max - min + 1) + min);
+            return rand;
+        }
     }
 });
--- app/services/session.js
+++ app/services/session.js
@@ -1,27 +1,33 @@
 app.factory('session', function GetSession($http, $q) {
-		return {
-			getConfig: function (configuration) {
-				var defer = $q.defer();
+    var dataset = {};
 
-				if (!configuration) {
-					configuration = "default";
-				}
-				$http({
-					url: "data/" + configuration + ".json",
-					method: "GET",
-					data: "{}",
-					headers: { 'Content-Type': 'application/json' }
-				}).success(function (data, status, headers, config) {
-					apphelper.setResolutions(data.resolutions);
-					apphelper.setConfiguration(data);
-					apphelper.setConfig("data/" + configuration + ".json");
-					defer.resolve('done');
-				}).error(function (data, status, headers, config) {
-					console.log(data);
-					defer.reject();
-				});
+    return {
+        getConfig: function (configuration) {
+            var defer = $q.defer();
 
-				return defer.promise;
-			}
-		}
-	});
+            if (!configuration) {
+                configuration = "default";
+            }
+            $http({
+                url: "data/" + configuration + ".json",
+                method: "GET",
+                data: "{}",
+                headers: { 'Content-Type': 'application/json' }
+            }).success(function (data, status, headers, config) {
+                setConfig(data);
+                defer.resolve('done');
+            }).error(function (data, status, headers, config) {
+                console.log(data);
+                defer.reject();
+            });
+
+            return defer.promise;
+        },
+        setDataset: function(data){
+            dataset = data;
+        },
+        getDataset: function(){
+            return dataset;
+        }
+    }
+});
